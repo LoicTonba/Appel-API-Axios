d4bb9de31b93e8c5df1e1954f6e773dd1e73ea28
Loic237 <144190628+LoicTonba@users.noreply.github.com>
2024-02-28 11:51:50 +0100

    Add files via upload

---
+++ src/components/Contenu.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container mt-5">
+
+    <h1 class="text-center">Appel à une API</h1>
+    <img :src="urlImg">
+    
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HContenu',
+  data: function(){
+    return {
+      urlImg: null
+    }
+  },
+ methods: {
+    
+   
+ },
+ mounted(){
+    axios
+     .get(' https://api.thecatapi.com/v1/images/search')
+     .then(reponse => {
+        console.log(reponse)
+        this.urlImg = reponse.data[0].url
+     })
+ },
+  components: {
+   
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1{
+    margin-top: 100px!important;
+  }
+  .onglets{
+    height: 300px;
+  }
+</style>
